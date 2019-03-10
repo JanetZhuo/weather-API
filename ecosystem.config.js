84d6f5e752f79891b02afc9be763e44ccf310e13
Janet Zhuo <janetzyf@JanetdeMBP.gateway>
2019-03-10 20:11:12 +1100

    modify env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "weather-app",
-      script: "./src/index.js",
-      instances: "max",
+      name: 'weather-app',
+      script: './src/index.js',
+      instances: 'max',
       env_production: {
-          NODE_ENV: "production"
+        NODE_ENV: 'production'
       }
     }
   ]
